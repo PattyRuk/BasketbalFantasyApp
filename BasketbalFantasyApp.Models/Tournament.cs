6507ba2572906f998164602f48bfd75c5dd7cc39
patrick rukundo <prukundo@mail.com>
2026-08-04 12:49:18 -0500

    tournament model created and filled

---
+++ BasketbalFantasyApp.Models/Tournament.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketbalFantasyApp.Models
+{
+    public class Tournament
+    {
+        public int TournamentId { get; set; }
+        public string TournamentName { get; set; } = string.Empty;
+        public string FormatType { get; set; } = string.Empty;
+        public DateTime EventDate { get; set; }
+
+        // Navigation property:  Many-to-Many connection
+        public List<TournamentPlayer> TournamentPlayers { get; set; } = new List<TournamentPlayer>();
+    }
+}
