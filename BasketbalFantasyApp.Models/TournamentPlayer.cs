d46d3a23e1c8651d5619d81daf8331c4de6e26e7
patrick rukundo <prukundo@mail.com>
2026-08-04 12:51:11 -0500

    tournament player model class added

---
+++ BasketbalFantasyApp.Models/TournamentPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketbalFantasyApp.Models
+{
+    public class TournamentPlayer
+    {
+        public int TournamentId { get; set; }
+        public Tournament Tournament { get; set; }
+
+        public int PlayerId { get; set; }
+        public Player Player { get; set; }
+
+        public int PointsScored { get; set; }
+        public int EfficiencyRating { get; set; }
+    }
+}
