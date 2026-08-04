c3a2f99c21f0f9b67ccc04473a1f596b893ea736
patrick rukundo <prukundo@mail.com>
2026-08-04 12:44:59 -0500

    player stats model added

---
+++ BasketbalFantasyApp.Models/PlayerStats.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketbalFantasyApp.Models
+{
+    public class PlayerStats
+    {
+        public int Id { get; set; }
+
+        // Foreign Key pointing back to the specific player profile
+        public int PlayerId { get; set; }
+
+        public DateTime GameDate { get; set; }
+
+        // Traditional Basketball Statistical Categories
+        public int Points { get; set; }
+        public int Rebounds { get; set; }
+        public int Assists { get; set; }
+        public int Steals { get; set; }
+        public int Blocks { get; set; }
+        public int Turnovers { get; set; }
+        public int ThreePointersMade { get; set; }
+        public double FieldGoalPercentage { get; set; }
+        public double FreeThrowPercentage { get; set; }
+
+        // Navigation property
+        public Player Player { get; set; }
+    }
+}
