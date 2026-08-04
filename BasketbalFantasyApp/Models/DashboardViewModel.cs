fb75fbfccaa835313e46ea80cb68ed41bb9f32e3
patrick rukundo <prukundo@mail.com>
2026-08-04 13:04:33 -0500

    dashboard oveview view model created

---
+++ BasketbalFantasyApp/Models/DashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BasketbalFantasyApp.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalTeamsCount { get; set; }
+        public int TotalPlayersCount { get; set; }
+        public int TotalTournamentsCount { get; set; }
+        public bool HasTeam { get; set; }
+        public string? UserTeamName { get; set; }
+        public string? UserTeamSponsor { get; set; }
+        public List<Player> MyRosterPlayers { get; set; } = new List<Player>();
+
+        public List<Tournament> UpcomingTournaments { get; set; } = new List<Tournament>();
+    }
+}
