3f2a0a87e657098d4b6be9987d0cd38157e78cb3
patrick rukundo <prukundo@mail.com>
2026-08-05 12:08:35 -0500

    initial logic for player controller with index view code

--- BasketbalFantasyApp/Controllers/PlayersController.cs
+++ BasketbalFantasyApp/Controllers/PlayersController.cs
@@ -10,6 +10,29 @@ namespace BasketbalFantasyApp.Controllers
     [Authorize]
     public class PlayersController : Controller
     {
+        private readonly BasketbalFantasyDbContext _database;
+
+        public PlayersController(BasketbalFantasyDbContext database)
+        {
+            _database = database;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var userTeam = await _database.Teams.FirstOrDefaultAsync(t => t.OwnerUserId == currentUserId);
+
+            if (userTeam == null)
+            {
+                return RedirectToAction("CreateTeam", "Teams");
+            }
+
+            var rosterList = await _database.Players
+                .Where(player => player.TeamId == userTeam.TeamId)
+                .ToListAsync();
+
+            return View(rosterList);
+        }
 
     }
 }
