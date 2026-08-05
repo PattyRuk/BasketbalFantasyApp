c12652466cb7891966e07f23f57349181fe3e975
patrick rukundo <prukundo@mail.com>
2026-08-05 12:02:42 -0500

    default team added to sample data

--- BasketbalFantasyApp/Data/SampleData.cs
+++ BasketbalFantasyApp/Data/SampleData.cs
@@ -5,6 +5,22 @@ namespace BasketbalFantasyApp.Data
 {
     public static class SampleData
     {
+        public static async Task SeedDatabaseAsync(BasketbalFantasyDbContext database)
+        {
+            // 1. Default initial pool team 
+            if (!database.Teams.Any())
+            {
+                var baseTeam = new Team
+                {
+                    TeamName = "League Free Agent Pool",
+                    SponsorName = "Global Sports Media",
+                    OwnerUserId = "SYSTEM_INITIAL_ACCOUNT"
+                };
+                database.Teams.Add(baseTeam);
+                await database.SaveChangesAsync();
+            }
 
+
+        }
     }
 }
