9dd4a2036ce4f290c222173cb43a9fb8868693a6
patrick rukundo <prukundo@mail.com>
2026-08-08 19:25:26 -0500

    tournaments controller created and intial code added for view/index

---
+++ BasketbalFantasyApp/Controllers/TournamentsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BasketbalFantasyApp.DAL;
+using BasketbalFantasyApp.Models;
+
+namespace BasketbalFantasyApp.Controllers
+{
+    public class TournamentsController : Controller
+    {
+        private readonly BasketbalFantasyDbContext _database;
+
+        public TournamentsController(BasketbalFantasyDbContext database)
+        {
+            _database = database;
+        }
+
+        [AllowAnonymous] // Anyone can view this list
+        public async Task<IActionResult> Index()
+        {
+            var scheduledBrackets = await _database.Tournaments
+                .OrderBy(tournament => tournament.EventDate)
+                .ToListAsync();
+
+            return View(scheduledBrackets);
+        }
+
+
+    }
+}
