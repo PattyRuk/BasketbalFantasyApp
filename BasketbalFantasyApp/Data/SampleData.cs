abccc1421d3fca71ed154dd01bfc8c83549c5d1b
patrick rukundo <prukundo@mail.com>
2026-08-10 11:05:51 -0500

    changed sample data logic to run without the baskeball api (currently causing isseus, no generated data)

--- BasketbalFantasyApp/Data/SampleData.cs
+++ BasketbalFantasyApp/Data/SampleData.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore; 
+﻿using Microsoft.EntityFrameworkCore;
 using BasketbalFantasyApp.DAL;
 using BasketbalFantasyApp.Models;
 using System;
@@ -12,62 +12,52 @@ namespace BasketbalFantasyApp.Data
     {
         public static async Task SeedDatabaseAsync(BasketbalFantasyDbContext database)
         {
-            // 1. Default initial pool team 
+            // 1. Seed the default System Team to hold the global unassigned players pool
             if (!await database.Teams.AnyAsync())
             {
-                var baseTeam = new Team
+                var systemPoolTeam = new Team
                 {
-                    TeamName = "League Free Agent Pool",
-                    SponsorName = "Global Sports Media",
-                    OwnerUserId = "SYSTEM_INITIAL_ACCOUNT"
+                    TeamId = 1, // Fixes the ID for lookups
+                    TeamName = "Global Player Pool",
+                    SponsorName = "League Collective",
+                    OwnerUserId = "SYSTEM_POOL"
                 };
 
-                database.Teams.Add(baseTeam);
+                database.Teams.Add(systemPoolTeam);
                 await database.SaveChangesAsync();
             }
 
-            // 2. Fetches data from the API if tables are empty 
+            // 2. Code-First Data Injection for the Basketball Players
             if (!await database.Players.AnyAsync())
             {
-                var placementTeam = await database.Teams.FirstOrDefaultAsync();
-
-                if (placementTeam != null)
+                var playersPoolList = new List<Player>
                 {
-                    var apiService = new BasketballApiService();
+                    new Player { Id = 101, FirstName = "LeBron", LastName = "James", Position = "Forward", NbaTeam = "Los Angeles Lakers", TeamId = 1, OwnerUserId = "SYSTEM_POOL" },
+                    new Player { Id = 102, FirstName = "Stephen", LastName = "Curry", Position = "Guard", NbaTeam = "Golden State Warriors", TeamId = 1, OwnerUserId = "SYSTEM_POOL" },
+                    new Player { Id = 103, FirstName = "Kevin", LastName = "Durant", Position = "Forward", NbaTeam = "Phoenix Suns", TeamId = 1, OwnerUserId = "SYSTEM_POOL" },
+                    new Player { Id = 104, FirstName = "Nikola", LastName = "Jokic", Position = "Center", NbaTeam = "Denver Nuggets", TeamId = 1, OwnerUserId = "SYSTEM_POOL" },
+                    new Player { Id = 105, FirstName = "Giannis", LastName = "Antetokounmpo", Position = "Forward", NbaTeam = "Milwaukee Bucks", TeamId = 1, OwnerUserId = "SYSTEM_POOL" },
+                    new Player { Id = 106, FirstName = "Luka", LastName = "Doncic", Position = "Guard", NbaTeam = "Dallas Mavericks", TeamId = 1, OwnerUserId = "SYSTEM_POOL" },
+                    new Player { Id = 107, FirstName = "Jayson", LastName = "Tatum", Position = "Forward", NbaTeam = "Boston Celtics", TeamId = 1, OwnerUserId = "SYSTEM_POOL" },
+                    new Player { Id = 108, FirstName = "Anthony", LastName = "Edwards", Position = "Guard", NbaTeam = "Minnesota Timberwolves", TeamId = 1, OwnerUserId = "SYSTEM_POOL" }
+                };
 
-                    // Active BallDon'tLie token key
-                    string activeApiKey = "2016ff56-3719-4c26-b7fe-d1facad5b1ce";
-                    var realPlayers = await apiService.FetchAndParseNbaPlayersAsync(activeApiKey, placementTeam.TeamId);
+                database.Players.AddRange(playersPoolList);
+                await database.SaveChangesAsync();
 
-                    if (realPlayers != null && realPlayers.Any())
-                    {
-                        database.Players.AddRange(realPlayers);
-                        await database.SaveChangesAsync();
+                // 3. Code-First Data Injection for Player Statistics
+                var sampleStatsList = new List<PlayerStats>
+                {
+                    new PlayerStats { PlayerId = 101, GameDate = DateTime.Now.AddDays(-1), Points = 26, Rebounds = 7, Assists = 8, Steals = 1, Blocks = 1, Turnovers = 3, ThreePointersMade = 2, FieldGoalPercentage = 0.520, FreeThrowPercentage = 0.780 },
+                    new PlayerStats { PlayerId = 102, GameDate = DateTime.Now.AddDays(-1), Points = 31, Rebounds = 4, Assists = 6, Steals = 2, Blocks = 0, Turnovers = 2, ThreePointersMade = 6, FieldGoalPercentage = 0.465, FreeThrowPercentage = 0.910 },
+                    new PlayerStats { PlayerId = 104, GameDate = DateTime.Now.AddDays(-1), Points = 28, Rebounds = 12, Assists = 10, Steals = 1, Blocks = 1, Turnovers = 4, ThreePointersMade = 1, FieldGoalPercentage = 0.580, FreeThrowPercentage = 0.820 }
+                };
 
-                        //sample statistic for newly downloaded players
-                        foreach (var player in database.Players.Take(5))
-                        {
-                            database.PlayerStats.Add(new PlayerStats
-                            {
-                                PlayerId = player.Id,
-                                GameDate = DateTime.Now.AddDays(-1),
-                                Points = 24,
-                                Rebounds = 8,
-                                Assists = 6,
-                                Steals = 2,
-                                Blocks = 1,
-                                Turnovers = 3,
-                                ThreePointersMade = 4,
-                                FieldGoalPercentage = 0.485,
-                                FreeThrowPercentage = 0.820
-                            });
-                        }
-                        await database.SaveChangesAsync();
-                    }
-                }
+                database.PlayerStats.AddRange(sampleStatsList);
+                await database.SaveChangesAsync();
             }
 
-            // 3. Sample upcoming tournament events if empty
+            // 4. Seed tournament events matching layout requirements
             if (!await database.Tournaments.AnyAsync())
             {
                 database.Tournaments.AddRange(new List<Tournament>
@@ -80,3 +70,4 @@ namespace BasketbalFantasyApp.Data
         }
     }
 }
+
