265c0612e61275be45b21e6d8c4c26d97cf93808
patrick rukundo <prukundo@mail.com>
2026-08-11 06:59:55 -0500

    new key enforcement migrated

---
+++ BasketbalFantasyApp.DAL/Migrations/20260811115839_EnforceManualKeyPrecedence.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BasketbalFantasyApp.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnforceManualKeyPrecedence : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
