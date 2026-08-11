7c3d8106d21043e859ae8090a28c5d2169e122ea
patrick rukundo <prukundo@mail.com>
2026-08-10 23:04:38 -0500

    available players view model created

---
+++ BasketbalFantasyApp/Models/AvailablePlayerViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BasketbalFantasyApp.Models
+{
+    public class AvailablePlayerViewModel
+    {
+        // Holds athletes currently unassigned to any custom managed team
+        public List<Player> AvailablePlayers { get; set; } = new List<Player>();
+    }
+}
+
