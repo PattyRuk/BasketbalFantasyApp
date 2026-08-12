d13be2ff03fc5a46d8cb03018d6759f675941dd5
patrick rukundo <prukundo@mail.com>
2026-08-11 19:29:32 -0500

    tournament team model junction class added

---
+++ BasketbalFantasyApp.Models/TournamentTeam.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketbalFantasyApp.Models
+{
+    public class TournamentTeam
+    {
+        public int TournamentId { get; set; }
+        public Tournament? Tournament { get; set; }
+
+        public int TeamId { get; set; }
+        public Team? Team { get; set; }
+
+        public int WinsCount { get; set; } = 0;
+        public int LossesCount { get; set; } = 0;
+        public string FinalPosition { get; set; } = "Contender"; // "Champion", "Runner-Up", "Eliminated"
+
+        // tournament-specific rosters/players
+        public List<Player> RegisteredPlayers { get; set; } = new List<Player>();
+    }
+}
