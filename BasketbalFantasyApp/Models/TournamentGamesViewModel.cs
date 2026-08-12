ee48d91904390c06dd3b8c2505dde8e19ede1def
patrick rukundo <prukundo@mail.com>
2026-08-12 14:09:02 -0500

    box score property added to tournament games view model

--- BasketbalFantasyApp/Models/TournamentGamesViewModel.cs
+++ BasketbalFantasyApp/Models/TournamentGamesViewModel.cs
@@ -4,6 +4,7 @@
     {
         public Tournament TournamentDetails { get; set; } = new Tournament();
         public List<Game> PlayedGames { get; set; } = new List<Game>();
+        public List<PlayerStats> TournamentPlayerBoxScores { get; set; } = new List<PlayerStats>();
     }
 }
 
