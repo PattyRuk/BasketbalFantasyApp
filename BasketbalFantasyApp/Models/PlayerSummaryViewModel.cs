38d2548b003e4747fc8c7a2519af0f91da44d40d
patrick rukundo <prukundo@mail.com>
2026-08-12 11:46:38 -0500

    player summary view model with a related attached tournamentstats class

---
+++ BasketbalFantasyApp/Models/PlayerSummaryViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace BasketbalFantasyApp.Models
+{
+    public class PlayerSummaryViewModel
+    {
+        public Player PlayerBio { get; set; } = new Player();
+        public Team DraftedTeam { get; set; } = new Team();
+
+        // Analytical Targets
+        public int TotalGamesPlayed { get; set; }
+        public int TotalPointsScored { get; set; }
+        public double AveragePointsPerGame { get; set; }
+        public double AverageRebounds { get; set; }
+        public double AverageAssists { get; set; }
+
+        // Tournament History Breakdown
+        public List<TournamentStatsRow> PerformanceHistory { get; set; } = new List<TournamentStatsRow>();
+    }
+
+    public class TournamentStatsRow
+    {
+        public string TournamentName { get; set; } = string.Empty;
+        public int GamesPlayed { get; set; }
+        public int PointsScored { get; set; }
+        public double FieldGoalPercentage { get; set; }
+    }
+}
