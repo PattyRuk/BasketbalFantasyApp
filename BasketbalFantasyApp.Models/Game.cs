9aae55a27af4a3b2980ca85f5fedabfad651833c
patrick rukundo <prukundo@mail.com>
2026-08-11 19:26:44 -0500

    game model class added with additions to tournament model

---
+++ BasketbalFantasyApp.Models/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketbalFantasyApp.Models
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public int TournamentId { get; set; }
+        public Tournament? Tournament { get; set; }
+
+        public int TeamAId { get; set; }
+        public Team? TeamA { get; set; }
+
+        public int TeamBId { get; set; }
+        public Team? TeamB { get; set; }
+
+        public int TeamAScore { get; set; }
+        public int TeamBScore { get; set; }
+        public DateTime MatchTimestamp { get; set; }
+
+        public int? WinnerTeamId { get; set; }
+    }
+}
--- BasketbalFantasyApp.Models/Tournament.cs
+++ BasketbalFantasyApp.Models/Tournament.cs
@@ -9,11 +9,23 @@ namespace BasketbalFantasyApp.Models
     public class Tournament
     {
         public int TournamentId { get; set; }
-        public string TournamentName { get; set; } = string.Empty;
-        public string FormatType { get; set; } = string.Empty;
+        public string? TournamentName { get; set; } 
+        public string? FormatType { get; set; }
         public DateTime EventDate { get; set; }
+
+        // Games required to win the tournament
+        public int RequiredWins { get; set; } = 3;
+        public bool IsCompleted { get; set; } = false;
+
+        // Championship Logs
+        public int? WinnerTeamId { get; set; }
+        public Team? WinnerTeam { get; set; }
+        public int? MvpPlayerId { get; set; }
+        public Player? MvpPlayer { get; set; }
 
         // Navigation property:  Many-to-Many connection
         public List<TournamentPlayer> TournamentPlayers { get; set; } = new List<TournamentPlayer>();
+        public List<Game> Games { get; set; } = new List<Game>();
+
     }
 }
