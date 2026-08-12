4a11bba70368713f2a1232d69be599aa0f7ac7ba
patrick rukundo <prukundo@mail.com>
2026-08-12 12:11:05 -0500

    new migrations added for the playerstats updates

--- BasketbalFantasyApp.DAL/Migrations/20260812171001_LinkTournamentToPlayerStats.Designer.cs
+++ BasketbalFantasyApp.DAL/Migrations/20260812171001_LinkTournamentToPlayerStats.Designer.cs
@@ -4,6 +4,7 @@ using BasketbalFantasyApp.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BasketbalFantasyApp.DAL.Migrations
 {
     [DbContext(typeof(BasketbalFantasyDbContext))]
-    partial class BasketbalFantasyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260812171001_LinkTournamentToPlayerStats")]
+    partial class LinkTournamentToPlayerStats
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BasketbalFantasyApp.DAL/Migrations/20260812171001_LinkTournamentToPlayerStats.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BasketbalFantasyApp.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class LinkTournamentToPlayerStats : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TournamentId",
+                table: "PlayerStats",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayerStats_TournamentId",
+                table: "PlayerStats",
+                column: "TournamentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PlayerStats_Tournaments_TournamentId",
+                table: "PlayerStats",
+                column: "TournamentId",
+                principalTable: "Tournaments",
+                principalColumn: "TournamentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PlayerStats_Tournaments_TournamentId",
+                table: "PlayerStats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PlayerStats_TournamentId",
+                table: "PlayerStats");
+
+            migrationBuilder.DropColumn(
+                name: "TournamentId",
+                table: "PlayerStats");
+        }
+    }
+}
