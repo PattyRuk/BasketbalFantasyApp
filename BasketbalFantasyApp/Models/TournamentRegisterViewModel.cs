5b94dbb21fdd732daa417cea90bc0b85e8718c92
patrick rukundo <prukundo@mail.com>
2026-08-11 19:33:10 -0500

    tournamnent register view midel created

---
+++ BasketbalFantasyApp/Models/TournamentRegisterViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BasketbalFantasyApp.Models
+{
+    public class TournamentRegisterViewModel
+    {
+        public int TournamentId { get; set; }
+        public string TournamentName { get; set; } = string.Empty;
+        public string FormatType { get; set; } = string.Empty;
+        public int RequiredPlayersCount { get; set; } // 3 or 5 based on layout constraints
+
+        public List<Player> EligibleRoster { get; set; } = new List<Player>();
+    }
+}
