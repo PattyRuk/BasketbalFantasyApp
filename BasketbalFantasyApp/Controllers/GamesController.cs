30e1ddafb479165424499db8be06b476a7f26cf4
patrick rukundo <prukundo@mail.com>
2026-08-11 23:24:10 -0500

    adding teams who will participate in, with hame object conting competing teams

--- BasketbalFantasyApp/Controllers/GamesController.cs
+++ BasketbalFantasyApp/Controllers/GamesController.cs
@@ -28,6 +28,41 @@ namespace BasketbalFantasyApp.Controllers
                 return RedirectToAction("Index", "Tournaments");
             }
 
+            var random = new Random();
+
+            // Run matchup loops until a contender team hits the win threshold
+            while (!tournament.IsCompleted)
+            {
+                var contenders = tournament.TournamentTeams.Where(tt => tt.FinalPosition == "Contender").ToList();
+                if (contenders.Count < 2) break;
+
+                //SEPARATE AND EXTRACT TWO DISTINCT TEAMS FROM THE POOL LIST
+                var teamA = contenders[random.Next(contenders.Count)];
+                var teamB = contenders.Where(t => t.TeamId != teamA.TeamId).ToList()[random.Next(contenders.Count - 1)];
+
+                int scoreA = random.Next(70, 115);
+                int scoreB = random.Next(70, 115);
+                while (scoreA == scoreB) scoreB = random.Next(70, 115);
+
+                var matchLog = new Game
+                {
+                    TournamentId = tournament.TournamentId,
+                    TeamAId = teamA.TeamId,
+                    TeamBId = teamB.TeamId,
+                    TeamAScore = scoreA,
+                    TeamBScore = scoreB,
+                    MatchTimestamp = DateTime.Now
+                };
+
+                var winner = scoreA > scoreB ? teamA : teamB;
+                var loser = scoreA > scoreB ? teamB : teamA;
+
+                matchLog.WinnerTeamId = winner.TeamId;
+                _database.Games.Add(matchLog);
+
+                winner.WinsCount++;
+                loser.LossesCount++;
+
 
 
             return RedirectToAction("Summary", "TournamentGameplay", new { id = tournament.TournamentId });
