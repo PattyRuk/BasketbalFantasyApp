e6500b978772ab2711f8a2677a46fb0f6e615ef7
patrick rukundo <prukundo@mail.com>
2026-08-11 19:36:52 -0500

    tournament summary view model created

---
+++ BasketbalFantasyApp/Models/TournamentSummaryViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace BasketbalFantasyApp.Models
+{
+    public class TournamentSummaryViewModel
+    {
+        public Tournament TournamentDetails { get; set; } = new Tournament();
+        public List<TournamentTeam> Standings { get; set; } = new List<TournamentTeam>();
+
+        // Winner specific highlights
+        public Team WinnerTeam { get; set; } = new Team();
+        public List<Player> WinnerRoster { get; set; } = new List<Player>();
+
+        // MVP parameters
+        public Player TournamentMvp { get; set; } = new Player();
+        public double MvpAveragePoints { get; set; }
+        public int MvpTotalPoints { get; set; }
+    }
+}
